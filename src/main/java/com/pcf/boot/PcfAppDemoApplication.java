303382d2d0f3ff57387b092422ccc8c635d8eb13
unknown <learning@DIN66007380.corp.capgemini.com>
2018-01-22 12:35:00 +0530

    added new spring boot project

---
+++ src/main/java/com/pcf/boot/PcfAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.pcf.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfAppDemoApplication.class, args);
+	}
+}
